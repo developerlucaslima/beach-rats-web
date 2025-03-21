348bb207bebe8e3d4e418e2595e74a4781276d5a
developerlucaslima <developerlucaslima@gmail.com>
2025-03-20 23:48:38 -0300

    feat: integrate ModeToggle into HeaderNavigation for improved theme management

--- src/components/header-navigation.tsx
+++ src/components/header-navigation.tsx
@@ -4,6 +4,7 @@ import { Button } from "./ui/button";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
 import Link from "next/link";
 import { useState } from "react";
+import { ModeToggle } from "./mode-toggle";
 
 export interface HeaderNavigationConfigProps {
   icon: LucideIcon
@@ -46,35 +47,39 @@ export function HeaderNavigation() {
             </Link>
           </Button>
         ))}
+        <ModeToggle />
       </nav>
 
       {/* mobile navigation */}
-      <Sheet open={isOpen} onOpenChange={setIsOpen}>
-        <SheetTrigger asChild className="md:hidden">
-          <Button variant="ghost" size="icon" aria-label="Menu">
-            <Menu className="h-5 w-5"/>
-            <span className="sr-only">Toggle menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="right" className="w-[240px] sm:w-[300px]">
-        <SheetHeader>
-          <SheetTitle className="hidden">My Hidden Title</SheetTitle>
-          <SheetDescription className="hidden">Additional description if needed.</SheetDescription>
-        </SheetHeader>
-          <div className="flex flex-col gap-4 py-4">
-            {headerNavigationConfig.map(item => (
-              <Button key={item.href} asChild variant='ghost' size='sm' className="justify-start" onClick={() => setIsOpen(false)}>
-                <Link href={item.href}>
-                  <item.icon className="mr-2 h-5 w-5"/>
-                  <span>
-                    {item.title}
-                  </span>
-                </Link>
-              </Button>
-            ))}
-          </div>
-        </SheetContent>
-      </Sheet>
+      <nav className="md:hidden flex gap-1">
+        <ModeToggle />
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger asChild>
+            <Button variant="ghost" size="icon" aria-label="Menu">
+              <Menu className="h-5 w-5"/>
+              <span className="sr-only">Toggle menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="right" className="w-[240px] sm:w-[300px]">
+            <SheetHeader>
+              <SheetTitle className="hidden">My Hidden Title</SheetTitle>
+              <SheetDescription className="hidden">Additional description if needed.</SheetDescription>
+            </SheetHeader>
+            <div className="flex flex-col gap-4 py-4">
+              {headerNavigationConfig.map(item => (
+                <Button key={item.href} asChild variant='ghost' size='sm' className="justify-start" onClick={() => setIsOpen(false)}>
+                  <Link href={item.href}>
+                    <item.icon className="mr-2 h-5 w-5"/>
+                    <span>
+                      {item.title}
+                    </span>
+                  </Link>
+                </Button>
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+      </nav>
     </>
   )
 }

32a0013f6319bd0d77f4360299b452a5f1a7baf6
developerlucaslima <developerlucaslima@gmail.com>
2025-03-22 18:27:19 -0300

    refactor: remove "use client" directive from multiple UI components

--- src/components/ui/skill-rating.tsx
+++ src/components/ui/skill-rating.tsx
@@ -1,9 +1,6 @@
-"use client";
-
 import * as React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
 import { cn } from "@/lib/utils";
-import { Button } from "./button";
 
 interface SkillRatingProps
   extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {

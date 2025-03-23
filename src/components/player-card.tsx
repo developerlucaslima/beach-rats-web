d412fe426297f8a78f1e552e67beed84fbfca916
developerlucaslima <developerlucaslima@gmail.com>
2025-03-23 17:06:05 -0300

    refactor: simplify PlayerCard component by removing role prop and enhancing attribute display logic

--- src/components/player-card.tsx
+++ src/components/player-card.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
 
-import type { CardType, Modality, RatingOption, Role } from '@/api/getPlayer'
+import type { CardType, Modality, RatingOption } from '@/api/getPlayer'
 import { cn } from '@/lib/utils'
 
 import { Card } from './ui/card'
 
 interface PlayerCardProps {
   name: string
-  role: Role
   modality: Modality
   photo: string
   country: string
@@ -23,7 +22,6 @@ interface PlayerCardProps {
 
 export default function PlayerCard({
   name,
-  role,
   modality,
   photo,
   country,
@@ -51,12 +49,25 @@ export default function PlayerCard({
     }
   }
 
-  const getAttributeColor = (value: number) => {
-    if (value >= 90) return 'text-green-500'
-    if (value >= 80) return 'text-lime-600'
-    if (value >= 70) return 'text-yellow-600'
-    if (value >= 60) return 'text-orange-500'
-    return 'text-red-500'
+  const attribute = (attribute: string, value: string | number) => {
+    let attributeColor = ''
+    if (typeof value === 'number') {
+      if (value >= 90) attributeColor = 'text-green-500'
+      if (value >= 75) attributeColor = 'text-lime-500'
+      if (value >= 60) attributeColor = 'text-orange-500'
+      else attributeColor = 'text-red-500'
+    }
+
+    return (
+      <div className="flex items-center justify-between">
+        <span className="text-sm font-semibold text-gray-800">{attribute}</span>
+        <div className="bg-input/30 border-input flex h-6 w-6 items-center justify-center rounded-sm border shadow-xs">
+          <span className={cn('text-sm font-bold', attributeColor)}>
+            {value}
+          </span>
+        </div>
+      </div>
+    )
   }
 
   return (
@@ -67,60 +78,21 @@ export default function PlayerCard({
           getCardGradient(),
         )}
       >
-        {/* Card Pattern */}
-        <div className="absolute inset-0 opacity-20">
-          <svg
-            width="100%"
-            height="100%"
-            viewBox="0 0 100 100"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <defs>
-              <pattern
-                id="smallGrid"
-                width="10"
-                height="10"
-                patternUnits="userSpaceOnUse"
-              >
-                <path
-                  d="M 10 0 L 0 0 0 10"
-                  fill="none"
-                  stroke="white"
-                  strokeWidth="0.5"
-                />
-              </pattern>
-              <pattern
-                id="grid"
-                width="100"
-                height="100"
-                patternUnits="userSpaceOnUse"
-              >
-                <rect width="100" height="100" fill="url(#smallGrid)" />
-                <path
-                  d="M 100 0 L 0 0 0 100"
-                  fill="none"
-                  stroke="white"
-                  strokeWidth="1"
-                />
-              </pattern>
-            </defs>
-            <rect width="100%" height="100%" fill="url(#grid)" />
-          </svg>
-        </div>
-
         {/* Card Content */}
-        <div className="relative z-10 flex h-full flex-col p-4">
+        <div className="relative z-10 flex h-full flex-col px-4">
           {/* Header */}
           <div className="mb-2 flex items-start justify-between">
+            <div className="flex flex-col">
+              <span className="text-sm font-semibold text-white">
+                {modality.toUpperCase()}
+              </span>
+              <span>{country}</span>
+            </div>
             <div className="flex flex-col items-center">
-              <div className="text-4xl font-bold text-white shadow-sm">
+              <div className="bg-input/30 border-input flex items-center justify-center rounded-sm border p-1 text-4xl font-bold text-white shadow-xs">
                 {overall}
               </div>
-              <div className="text-sm font-semibold text-white">
-                {role.toUpperCase()}
-              </div>
             </div>
-            <div>{country}</div>
           </div>
 
           {/* Player Image */}
@@ -130,7 +102,7 @@ export default function PlayerCard({
                 src={photo || '/placeholder.svg'}
                 alt={name}
                 fill
-                className="object-cover"
+                className="rounded-sm object-cover"
                 style={{ objectPosition: 'top center' }}
               />
             </div>
@@ -143,73 +115,17 @@ export default function PlayerCard({
 
           {/* Attributes */}
           <div className="grid grid-cols-2 gap-x-4 gap-y-1">
-            <div className="flex justify-between">
-              <span className="text-xs font-semibold text-gray-800">
-                ATAQUE
-              </span>
-              <span
-                className={cn('text-xs font-bold', getAttributeColor(attack))}
-              >
-                {attack}
-              </span>
-            </div>
-            <div className="flex justify-between">
-              <span className="text-xs font-semibold text-gray-800">
-                DEFESA
-              </span>
-              <span
-                className={cn('text-xs font-bold', getAttributeColor(defense))}
-              >
-                {defense}
-              </span>
-            </div>
-            <div className="flex justify-between">
-              <span className="text-xs font-semibold text-gray-800">
-                FUNDAMENTO
-              </span>
-              <span
-                className={cn(
-                  'text-xs font-bold',
-                  getAttributeColor(fundamental),
-                )}
-              >
-                {fundamental}
-              </span>
-            </div>
-            <div className="flex justify-between">
-              <span className="text-xs font-semibold text-gray-800">
-                RECURSO
-              </span>
-              <span
-                className={cn('text-xs font-bold', getAttributeColor(resource))}
-              >
-                {resource}
-              </span>
-            </div>
-            <div className="flex justify-between">
-              <span className="text-xs font-semibold text-gray-800">
-                FÍSICO
-              </span>
-              <span className={'text-xs font-bold'}>{physical.emoji}</span>
-            </div>
-            <div className="flex justify-between">
-              <span className="text-xs font-semibold text-gray-800">
-                MENTAL
-              </span>
-              <span className={'text-xs font-bold'}>{mental.emoji}</span>
-            </div>
-          </div>
-
-          {/* Footer */}
-          <div className="mt-auto flex justify-center space-x-2">
-            <span className="text-sm font-bold text-white">
-              {modality.toUpperCase()}
-            </span>
+            {attribute('ATQ', attack)}
+            {attribute('DEF', defense)}
+            {attribute('FUN', fundamental)}
+            {attribute('REC', resource)}
+            {attribute('FIS', physical.emoji)}
+            {attribute('MEN', mental.emoji)}
           </div>
         </div>
 
         {/* Card Shine Effect */}
-        <div className="absolute inset-0 bg-gradient-to-tr from-white/0 via-white/30 to-white/0 opacity-30"></div>
+        <div className="opacity-5s0 absolute inset-0 bg-gradient-to-bl from-white/0 via-white/30 to-white/30"></div>
       </Card>
     </div>
   )

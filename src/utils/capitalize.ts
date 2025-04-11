f97482d04812054a114828e06ee608fad2a59817
developerlucaslima <developerlucaslima@gmail.com>
2025-04-11 12:28:04 -0300

    feat: add capitalizeEachFirstLetter utility function

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,3 +1,10 @@
 export const capitalizeFirstLetter = (str: string): string => {
-  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+}
+
+export const capitalizeEachFirstLetter = (str: string): string => {
+  return str
+    .split(' ')
+    .map((word) => capitalizeFirstLetter(word))
+    .join(' ')
 }

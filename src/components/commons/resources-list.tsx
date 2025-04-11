fefb429d21c7730a83ba2fee3191bf5aea1472d2
developerlucaslima <developerlucaslima@gmail.com>
2025-04-11 17:51:46 -0300

    refactor: simplify FundamentalsList and ResourcesList components by removing unnecessary Card wrappers

--- src/components/commons/resources-list.tsx
+++ src/components/commons/resources-list.tsx
@@ -8,13 +8,7 @@ import {
   AccordionTrigger,
 } from '../ui/accordion'
 import { Badge } from '../ui/badge'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '../ui/card'
+import { CardContent } from '../ui/card'
 import { StarsProgress } from '../ui/stars-progress'
 
 interface ResourcesListProps {
@@ -23,67 +17,48 @@ interface ResourcesListProps {
 
 export const ResourcesList = ({ resources }: ResourcesListProps) => {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Recursos</CardTitle>
-        <CardDescription>
-          Manobras avançadas e fintas que enriquecem o jogador, tornando-o mais
-          imprevisível e deixando o jogo mais divertido.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <ul className="space-y-2">
-          {resources.map((resource) => (
-            <li key={resource.sluggedName}>
-              <Accordion
-                type="single"
-                collapsible
-                className="rounded-md border"
-              >
-                <AccordionItem value={resource.sluggedName}>
-                  <div className="flex flex-col gap-2 p-4">
-                    <div className="flex items-center justify-between">
-                      <div className="font-medium">{resource.name}</div>
-                      <div className="flex items-center gap-2">
-                        <Badge variant="outline">
-                          {capitalizeFirstLetter(
-                            resource.progressionDescription,
-                          )}
-                        </Badge>
+    <CardContent>
+      <ul className="space-y-2">
+        {resources.map((resource) => (
+          <li key={resource.sluggedName}>
+            <Accordion type="single" collapsible className="rounded-md border">
+              <AccordionItem value={resource.sluggedName}>
+                <div className="flex flex-col gap-2 p-4">
+                  <div className="flex items-center justify-between">
+                    <div className="font-medium">{resource.name}</div>
+                    <div className="flex items-center gap-2">
+                      <Badge variant="outline">
+                        {capitalizeFirstLetter(resource.progressionDescription)}
+                      </Badge>
 
-                        <AccordionTrigger className="cursor-pointer p-0">
-                          <span className="sr-only">Accordion toggle</span>
-                        </AccordionTrigger>
-                      </div>
+                      <AccordionTrigger className="cursor-pointer p-0">
+                        <span className="sr-only">Accordion toggle</span>
+                      </AccordionTrigger>
                     </div>
-                    <StarsProgress
-                      maxStars={3}
-                      filledStars={resource.progressionValue}
-                      size={18}
-                    />
                   </div>
-                  <AccordionContent className="flex flex-col gap-2 px-4 pb-2">
-                    <div className="text-muted-foreground text-sm">
-                      {resource.description}
-                    </div>
-                    <div className="ml-auto">
-                      {resource.skillType.map((type) => (
-                        <Badge
-                          variant="secondary"
-                          className="text-xs"
-                          key={type}
-                        >
-                          {capitalizeFirstLetter(type)}
-                        </Badge>
-                      ))}
-                    </div>
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-            </li>
-          ))}
-        </ul>
-      </CardContent>
-    </Card>
+                  <StarsProgress
+                    maxStars={3}
+                    filledStars={resource.progressionValue}
+                    size={18}
+                  />
+                </div>
+                <AccordionContent className="flex flex-col gap-2 px-4 pb-2">
+                  <div className="text-muted-foreground text-sm">
+                    {resource.description}
+                  </div>
+                  <div className="ml-auto">
+                    {resource.skillType.map((type) => (
+                      <Badge variant="secondary" className="text-xs" key={type}>
+                        {capitalizeFirstLetter(type)}
+                      </Badge>
+                    ))}
+                  </div>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </li>
+        ))}
+      </ul>
+    </CardContent>
   )
 }

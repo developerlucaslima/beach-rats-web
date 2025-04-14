c803525eaf26e0d88710a6e3db2a93c5fcfefd64
developerlucaslima <developerlucaslima@gmail.com>
2025-04-14 15:26:51 -0300

    feat: add ProfileAvatar component and dropdown menu for user profile actions

---
+++ src/components/profile-dropdown/profile-avatar.tsx
@@ -0,0 +1,10 @@
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
+
+export const ProfileAvatar = () => {
+  return (
+    <Avatar className="cursor-pointer">
+      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+      <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
+  )
+}

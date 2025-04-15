7e7ca6f13c562107b38efe30664fc886ca3f7904
developerlucaslima <developerlucaslima@gmail.com>
2025-04-15 11:16:21 -0300

    feat: add PlayerCard component with configuration and layout for player attributes

---
+++ src/components/player-card/config.ts
@@ -0,0 +1,61 @@
+import type {
+  CardType,
+  CategoryProgression,
+  LabelAndEmoji,
+  Modality,
+} from '@/types'
+
+import type { PlayerCardAtributeProps } from '../ui/player-card-composition'
+
+export interface PlayerCardConfig {
+  modality: Modality
+  cardType: CardType
+  currentCategory: CategoryProgression
+  overall: number
+  name: string
+  profileImage: string
+  country: LabelAndEmoji
+  stars: number
+  atributesArray: PlayerCardAtributeProps[]
+}
+
+const mock = {
+  country: { label: 'Brasil', emoji: '🇧🇷' },
+  physicalCondition: { label: 'Excelente', emoji: '💪' },
+  mentalCondition: { label: 'Focado', emoji: '🧠' },
+  attack: 90,
+  defense: 75,
+  fundamental: 88,
+  resource: 82,
+}
+
+export const playerCardConfig: PlayerCardConfig = {
+  modality: 'futevôlei',
+  cardType: 'silver',
+  currentCategory: 'intermediário',
+  overall: 85,
+  name: 'Lucas Lima',
+  profileImage: 'https://github.com/developerlucaslima.png',
+  country: mock.country,
+  stars: 3,
+  atributesArray: [
+    { label: 'Média de ataque', abbreviation: 'ATA', value: mock.attack },
+    { label: 'Média de defesa', abbreviation: 'DEF', value: mock.defense },
+    {
+      label: 'Média dos fundamentos',
+      abbreviation: 'FUN',
+      value: mock.fundamental,
+    },
+    { label: 'Média dos recursos', abbreviation: 'REC', value: mock.resource },
+    {
+      label: `Físico: ${mock.physicalCondition.label}`,
+      abbreviation: 'FÍS',
+      value: mock.physicalCondition.emoji,
+    },
+    {
+      label: `Mental: ${mock.mentalCondition.label}`,
+      abbreviation: 'MEN',
+      value: mock.mentalCondition.emoji,
+    },
+  ]
+}
---
+++ src/components/player-card/index.tsx
@@ -0,0 +1,83 @@
+import Image from 'next/image'
+
+import {
+  PlayerCardAtribute,
+  PlayerCardAtributes,
+  PlayerCardBadge,
+  PlayerCardComposition,
+  PlayerCardEmoji,
+  PlayerCardFooter,
+  PlayerCardHeader,
+  PlayerCardImage,
+  PlayerCardName,
+  PlayerCardOverall,
+  PlayerCardTitle,
+  PlayerCardTopLeft,
+  PlayerCardTopRight,
+} from '@/components/ui/player-card-composition'
+import { StarsProgress } from '@/components/ui/stars-progress'
+import { cn } from '@/utils/cn'
+
+import { playerCardConfig } from './config'
+
+export const PlayerCard = () => {
+  const {
+    modality,
+    cardType,
+    currentCategory,
+    overall,
+    name,
+    profileImage,
+    stars,
+    country,
+    atributesArray
+  } = playerCardConfig
+
+  return (
+    <PlayerCardComposition cardType={cardType} className={cn('relative')}>
+      <PlayerCardHeader>
+        <PlayerCardTopLeft>
+          <PlayerCardOverall>{overall}</PlayerCardOverall>
+        </PlayerCardTopLeft>
+        <PlayerCardTopRight>
+          <PlayerCardBadge cardType={cardType}>
+            {modality.toUpperCase() + ' - ' + currentCategory.toUpperCase()}
+          </PlayerCardBadge>
+          <PlayerCardEmoji>{country.emoji}</PlayerCardEmoji>
+        </PlayerCardTopRight>
+      </PlayerCardHeader>
+
+      <PlayerCardImage>
+        <Image
+          src={profileImage}
+          alt={name}
+          fill
+          className="object-cover"
+        />
+      </PlayerCardImage>
+
+      <PlayerCardTitle>
+        <PlayerCardName>{name.toUpperCase()}</PlayerCardName>
+      </PlayerCardTitle>
+
+      <PlayerCardAtributes>
+        {atributesArray.map(({ abbreviation, label, value }) => (
+          <PlayerCardAtribute
+            key={abbreviation}
+            abbreviation={abbreviation}
+            label={label}
+            value={value}
+          />
+        ))}
+      </PlayerCardAtributes>
+      <PlayerCardFooter>
+        <StarsProgress
+          maxStars={3}
+          filledStars={stars}
+          size={20}
+          className="text-background/80"
+        />
+      </PlayerCardFooter>
+    </PlayerCardComposition>
+  )
+}

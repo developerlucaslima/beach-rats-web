53cd74a71b167d66c235ec16cbea349f5b41e4ae
developerlucaslima <developerlucaslima@gmail.com>
2025-05-12 14:54:44 -0300

    feat: create Providers component with QueryClient and Toaster integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import '@/styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-import { GoogleScript } from '@/config/google/google-script'
 import { Providers } from '@/config/providers'
+import { GoogleScript } from '@/config/services/google/google-script'
 
 const inter = Inter({
   variable: '--font-inter',

e718458d97cb97f991923d50837c59a98a919225
developerlucaslima <developerlucaslima@gmail.com>
2025-05-12 11:49:40 -0300

    fix: update logo image source to correct path for Beach Rats logo

--- src/components/commons/logo.tsx
+++ src/components/commons/logo.tsx
@@ -7,7 +7,7 @@ export const BeachRatsLogo = () => (
   <div className="flex items-center gap-2">
     <Link href="/" className="flex items-center space-x-2">
       <Avatar className="cursor-pointer size-10 rounded-xl">
-        <AvatarImage src="/beach-rats-logo.png" alt="Beach rats logo" />
+        <AvatarImage src="/sports-rats-logo.png" alt="Beach rats logo" />
         <AvatarFallback>BR</AvatarFallback>
       </Avatar>
       <span className="font-bold hidden sm:inline-block">Beach Rats</span>

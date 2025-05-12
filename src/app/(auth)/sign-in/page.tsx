411d377e5ad70a2acda0b9e5916e819a4127cf09
developerlucaslima <developerlucaslima@gmail.com>
2025-05-12 15:51:22 -0300

    feat: replace SignInHeader with GoogleOneTap and add AuthHeader component for improved sign-in experience

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,12 +1,13 @@
+import { AuthHeader } from "@/features/auth/components/auth-header";
+import { GoogleOneTap } from "@/features/auth/components/google-one-tap";
 import { SignInForm } from "@/features/auth/components/sign-in-form";
-import { SignInHeader } from "@/features/auth/components/sign-in-header";
 
 export default function SignIn() {
   return (
-    // mobile fist
     <div className="flex items-center justify-center px-4 py-12 sm:px-6 lg:px-8 mx-auto">
       <div className="w-full max-w-md space-y-8">
-        <SignInHeader />
+        <GoogleOneTap />
+        <AuthHeader title="Acesse sua conta" linkText="crie uma nova conta" linkHref="/sign-up"/>
         <SignInForm />
       </div>
     </div>
